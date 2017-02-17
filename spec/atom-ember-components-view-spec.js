d6a9be8ed0e2f52e5bee321f8d7864a29614b244
Parsa Alipour <palipour@pagerduty.com>
2017-02-16 21:18:49 -0800

    initial commit

---
+++ spec/atom-ember-components-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomEmberComponentsView from '../lib/atom-ember-components-view';
+
+describe('AtomEmberComponentsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
